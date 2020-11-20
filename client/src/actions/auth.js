6b2efe9bdfb525922a2c2e05ffb3a185645d93eb
Ilvir Zakiryanov <zakilvir@gmail.com>
2020-11-20 15:56:01 +0500

    Logout & Navbar Links

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import setAuthToken from '../utils/setAuthToken';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from './types';
 
 
 // Load User
@@ -87,5 +87,10 @@ export const login = (email, password) => async dispatch => {
             type: LOGIN_FAIL
         });
     }
+}
+
+// Logout / Clear profile
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT });
 }
 
